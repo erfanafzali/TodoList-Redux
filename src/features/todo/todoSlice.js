f440ab36a214b0c144937704a35159a8da275531
INVOKER <developer.invoker@gmail.com>
2023-12-08 23:33:37 +0330

    update extraReduser RTK 2

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,15 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// http://localhost:5000/todos
-
 const api = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 export const getAsyncTodos = createAsyncThunk(
   "todos/getAsyncTodos",
-  async (payload, { rejectWithValue }) => {
+  async (_, { rejectWithValue }) => {
     try {
       const response = await api.get("/todos");
       return response.data;
@@ -89,44 +87,87 @@ const todoSlice = createSlice({
       );
     },
   },
-  extraReducers: {
-    [getAsyncTodos.pending]: (state) => {
+  extraReducers: (builder) => {
+    builder.addCase(getAsyncTodos.pending, (state) => {
       state.loading = true;
       state.todos = [];
       state.error = "";
-    },
-    [getAsyncTodos.fulfilled]: (state, action) => {
+    });
+
+    builder.addCase(getAsyncTodos.fulfilled, (state, action) => {
       state.loading = false;
       state.todos = action.payload;
       state.error = "";
-    },
-    [getAsyncTodos.rejected]: (state, action) => {
+    });
+
+    builder.addCase(getAsyncTodos.rejected, (state, action) => {
       state.loading = false;
       state.todos = [];
       state.error = action.payload;
-    },
-    [addAsyncTodo.pending]: (state) => {
+    });
+
+    builder.addCase(addAsyncTodo.pending, (state) => {
       state.loading = true;
-    },
-    [addAsyncTodo.fulfilled]: (state, action) => {
+    });
+
+    builder.addCase(addAsyncTodo.fulfilled, (state, action) => {
       state.loading = false;
       state.todos.push(action.payload);
-    },
-    [deleteAsyncTodo.fulfilled]: (state, action) => {
+    });
+
+    builder.addCase(deleteAsyncTodo.fulfilled, (state, action) => {
       state.loading = false;
       state.todos = state.todos.filter(
         (todo) => todo.id !== Number(action.payload.id)
       );
-    },
-    [toggleAsyncTodo.fulfilled]: (state, action) => {
+    });
+
+    builder.addCase(toggleAsyncTodo.fulfilled, (state, action) => {
       const selectedTodo = state.todos.find(
         (todo) => todo.id === Number(action.payload.id)
       );
       selectedTodo.completed = action.payload.completed;
-    },
+    });
   },
 });
 
 export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
+
+// extraReducers: {
+//   [getAsyncTodos.pending]: (state) => {
+//     state.loading = true;
+//     state.todos = [];
+//     state.error = "";
+//   },
+//   [getAsyncTodos.fulfilled]: (state, action) => {
+//     state.loading = false;
+//     state.todos = action.payload;
+//     state.error = "";
+//   },
+//   [getAsyncTodos.rejected]: (state, action) => {
+//     state.loading = false;
+//     state.todos = [];
+//     state.error = action.payload;
+//   },
+//   [addAsyncTodo.pending]: (state) => {
+//     state.loading = true;
+//   },
+//   [addAsyncTodo.fulfilled]: (state, action) => {
+//     state.loading = false;
+//     state.todos.push(action.payload);
+//   },
+//   [deleteAsyncTodo.fulfilled]: (state, action) => {
+//     state.loading = false;
+//     state.todos = state.todos.filter(
+//       (todo) => todo.id !== Number(action.payload.id)
+//     );
+//   },
+//   [toggleAsyncTodo.fulfilled]: (state, action) => {
+//     const selectedTodo = state.todos.find(
+//       (todo) => todo.id === Number(action.payload.id)
+//     );
+//     selectedTodo.completed = action.payload.completed;
+//   },
+// },
